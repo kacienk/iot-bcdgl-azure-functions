b5a1e18d81dd6c6fdb244fe281de1b1d37cf7bc7
kacienk <ktcienkosz@gmail.com>
2024-01-07 15:17:52 +0100

    add device data model

---
+++ DeviceData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Iotbcdg.Model
+{
+    public class DeviceData
+    {
+        public string Id { get; set; }
+        public double Value { get; set; }
+    }
+}
