f817641ec2f991cdfb18e4cf695d4f0372302b29
kacienk <ktcienkosz@gmail.com>
2024-01-26 01:34:59 +0100

    remove comments

--- models/GoogleUserData.cs
+++ models/GoogleUserData.cs
@@ -1,16 +1,3 @@
-/* 
-{
-[2024-01-14T21:44:06.519Z]   "sub": "104594561722614628791",
-[2024-01-14T21:44:06.519Z]   "name": "Miłosz D",
-[2024-01-14T21:44:06.519Z]   "given_name": "Miłosz",
-[2024-01-14T21:44:06.519Z]   "family_name": "D",
-[2024-01-14T21:44:06.519Z]   "picture": "https://lh3.googleusercontent.com/a/ACg8ocLysXv4i_yGXM7diLt46NxJZptVhpRTjSZnluBejqVT\u003ds96-c",
-[2024-01-14T21:44:06.519Z]   "email": "midub@tlen.pl",
-[2024-01-14T21:44:06.519Z]   "email_verified": true,
-[2024-01-14T21:44:06.520Z]   "locale": "pl"
-[2024-01-14T21:44:06.520Z] }
-*/
-
 using System.Runtime.Serialization;
 
 namespace Iotbcdg.Model
